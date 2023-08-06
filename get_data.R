ab9d18ad8f98e902a48aa28ea630a140b4e47151
Susan Vanderplas <srvanderplas@gmail.com>
2023-08-06 09:00:14 -0500

    Updates for Stat 850 in fall 23

--- get_data.R
+++ get_data.R
@@ -12,9 +12,9 @@ poems <- sprog_comments$poem_for_your_sprog$comments %>%
   mutate(comment = str_replace_all(comment, "&gt;", "> ") %>%
            str_replace_all("\\031", "'"))
 
-write.csv(poems, file = "sprog_poems.csv", row.names = F)
+write.csv(poems, file = "sprog_poems-20230806.csv", row.names = F)
 
 poems <- schnoodle_comments$SchnoodleDoodleDo$comments %>%
   mutate(comment = str_replace_all(comment, "&gt;", "> ") %>%
            str_replace_all("\\031", "'"))
-write.csv(poems, file = "schnoodle_poems.csv", row.names = F)
+write.csv(poems, file = "schnoodle_poems-20230806.csv", row.names = F)
